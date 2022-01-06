fc52ffe14a08ca44489a32a8bb50c6cd0e8e2bc5
Arraktur <arrakturil@outlook.com>
2022-01-06 19:41:08 +0200

    fix

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Paracticum fourth project. Social App for sharing pictures - the first part of t
 ---
 
 ###  Technologi specifications
+---
 
 In the project used flex and grid layouts and media queries for adaptive layouts.
 The site have profile edit form. JS used for interaction of the user with the site profile and like buttons.
--- scripts/index.js
+++ scripts/index.js
@@ -4,50 +4,41 @@ let closeButton = document.querySelector(".popup__close");
 
 let editForm = document.querySelector(".popup");
 
-function openEditForm() {
-    editForm.classList.remove("popup_hidden");
-}
-
-editButton.addEventListener("click", openEditForm);
-
-function closeEditForm() {
-    editForm.classList.add("popup_hidden");
-}
-
-closeButton.addEventListener("click", closeEditForm);
-
-
 let formElement = document.querySelector(".edit-form");
 
 let nameInput = document.querySelector("#name");
 let aboutInput = document.querySelector("#about");
 
-nameInput.value = 'Jacques Cousteau'
-aboutInput.value = 'Explorer'
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__about");
 
+nameInput.value = profileName.textContent;
+aboutInput.value = profileAbout.textContent;
 
+function closeEditForm() {
+    editForm.classList.add("popup_hidden");
+}
+
+function openEditForm() {
+    editForm.classList.remove("popup_hidden");
+}
 
 function handleProfileFormSubmit(evt) {
-   
     evt.preventDefault();
-    
-    let nameValue = nameInput.value
-    let profileName = document.querySelector('.profile__name')
-    profileName.textContent = nameValue
-   
-    let aboutValue = aboutInput.value
-    let profileAbout = document.querySelector('.profile__about')
-    profileAbout.textContent = aboutValue
+
+    let nameValue = nameInput.value;
+
+    profileName.textContent = nameValue;
+
+    let aboutValue = aboutInput.value;
+
+    profileAbout.textContent = aboutValue;
 
     closeEditForm();
 }
 
-
 formElement.addEventListener("submit", handleProfileFormSubmit);
 
-let likeButton = document.querySelectorAll('.element__post-like')
-for (let i = 0; i < likeButton.length; i++) {
-    likeButton[i].addEventListener('click', function () {
-        likeButton[i].classList.toggle('element__post-like')
-    }) 
-}
+editButton.addEventListener("click", openEditForm);
+
+closeButton.addEventListener("click", closeEditForm);
